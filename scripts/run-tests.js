9a4064212602fd1491f1a607d5ff7746ecb0b3eb
Artur Adib <arturadib@gmail.com>
2012-03-02 11:38:34 -0500

    run-tests

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 require('../global');
 
+var failed = false;
+
 cd(__dirname + '/../test');
 for (file in ls('*.js')) {
   echo('Running test:', file);
-  exec('node '+file);
+  if (exec('node '+file).code !== 123) // 123 avoids false positives (e.g. premature exit)
+    failed = true;
+}
+
+if (failed) {
+  echo();
+  echo('WARNING: Some tests did not pass!');
+  exit(1);
+} else {
+  echo();
+  echo('All tests passed.');
 }
