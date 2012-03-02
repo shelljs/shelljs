59e9bbfaa0b316cdfcbedbbb81d1926022da67e4
Artur Adib <arturadib@gmail.com>
2012-03-01 21:13:24 -0500

    cat

--- test/cat.js
+++ test/cat.js
@@ -1,4 +1,4 @@
-require('../maker');
+var shell = require('..');
 
 var assert = require('assert'),
     path = require('path'),
@@ -7,50 +7,43 @@ var assert = require('assert'),
 // Node shims for < v0.7
 fs.existsSync = fs.existsSync || path.existsSync;
 
-silent();
+shell.silent();
 
 function numLines(str) {
   return typeof str === 'string' ? str.match(/\n/g).length : 0;
 }
 
 // save current dir
-var cur = pwd();
+var cur = shell.pwd();
 
 //
 // Invalids
 //
 
-cat();
-assert.ok(error());
+shell.cat();
+assert.ok(shell.error());
 
 assert.equal(fs.existsSync('/asdfasdf'), false); // sanity check
-cat('/adsfasdf'); // file does not exist
-assert.ok(error());
+shell.cat('/adsfasdf'); // file does not exist
+assert.ok(shell.error());
 
 //
 // Valids
 //
 
-assert.equal(cat, read); // should be synonyms
-
 // simple
-var result = cat('resources/file1');
-assert.equal(error(), null);
+var result = shell.cat('resources/file1');
+assert.equal(shell.error(), null);
 assert.equal(result, 'test1');
 
 // multiple files
-var result = cat('resources/file2 resources/file1');
-assert.equal(error(), null);
+var result = shell.cat('resources/file2', 'resources/file1');
+assert.equal(shell.error(), null);
 assert.equal(result, 'test2\ntest1');
 
-// multiple files, comma-syntax
-var result = cat('resources/file2', 'resources/file1');
-assert.equal(error(), null);
-assert.equal(result, 'test2\ntest1');
-
-var result = cat('resources/file*.txt');
-assert.equal(error(), null);
+var result = shell.cat('resources/file*.txt');
+assert.equal(shell.error(), null);
 assert.ok(result.search('test1') > -1); // file order might be random
 assert.ok(result.search('test2') > -1);
 
-exit(123);
+shell.exit(123);
