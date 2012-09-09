8a46ec6c8353bfc4ad0a6301d236659dd4f8e6fe
James Campos <james.r.campos@gmail.com>
2012-09-08 19:14:35 -0700

    add test tests (causes mv tests to fail

--- test/test.js
+++ test/test.js
@@ -32,20 +32,159 @@ assert.ok(shell.error());
 // Valids
 //
 
-var result = shell.test('-f', 'resources/file1');
+//exists
+var result = shell.test('-e', 'resources/file1');
 assert.equal(shell.error(), null);
 assert.equal(result, true);
 
-var result = shell.test('-d', 'resources/file1');
+//block
+var result = shell.test('-b', 'resources/block');
+assert.equal(shell.error(), null);
+assert.equal(result, true);//true
+
+var result = shell.test('-c', 'resources/block');
 assert.equal(shell.error(), null);
 assert.equal(result, false);
 
-var result = shell.test('-f', 'resources');
+var result = shell.test('-d', 'resources/block');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-f', 'resources/block');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-L', 'resources/block');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-p', 'resources/block');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+//character
+var result = shell.test('-b', 'resources/character');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-c', 'resources/character');
+assert.equal(shell.error(), null);
+assert.equal(result, true);//true
+
+var result = shell.test('-d', 'resources/character');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-f', 'resources/character');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-L', 'resources/character');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-p', 'resources/character');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+//directory
+var result = shell.test('-b', 'resources');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-c', 'resources');
 assert.equal(shell.error(), null);
 assert.equal(result, false);
 
 var result = shell.test('-d', 'resources');
 assert.equal(shell.error(), null);
-assert.equal(result, true);
+assert.equal(result, true);//true
+
+var result = shell.test('-f', 'resources');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-L', 'resources');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-p', 'resources');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+//file
+var result = shell.test('-b', 'resources/file1');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-c', 'resources/file1');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-d', 'resources/file1');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-f', 'resources/file1');
+assert.equal(shell.error(), null);
+assert.equal(result, true);//true
+
+var result = shell.test('-L', 'resources/file1');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-p', 'resources/file1');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+//link
+var result = shell.test('-b', 'resources/link');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-c', 'resources/link');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-d', 'resources/link');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-f', 'resources/link');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-L', 'resources/link');
+assert.equal(shell.error(), null);
+assert.equal(result, true);//true
+
+var result = shell.test('-p', 'resources/link');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+//pipe
+var result = shell.test('-b', 'resources/pipe');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-c', 'resources/pipe');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-d', 'resources/pipe');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-f', 'resources/pipe');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-L', 'resources/pipe');
+assert.equal(shell.error(), null);
+assert.equal(result, false);
+
+var result = shell.test('-p', 'resources/pipe');
+assert.equal(shell.error(), null);
+assert.equal(result, true);//true
 
 shell.exit(123);
