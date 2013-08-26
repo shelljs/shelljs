ff5212aa97b3356caf8bc92b7a88a9592145dedb
Andrew Duffy <andreweduffy@gmail.com>
2013-08-25 19:27:58 -0700

    Updated test for toEnd() to be more comprehensive.

--- test/toEnd.js
+++ test/toEnd.js
@@ -24,17 +24,17 @@ shell.mkdir('tmp');
 assert.ok(shell.error());
 
 assert.equal(fs.existsSync('/asdfasdf'), false); // sanity check
-'hello world'.toEnd('/asdfasdf/file');
 assert.ok(shell.error());
-
 //
 // Valids
 //
 
+assert.equal(fs.existsSync('tmp/toEnd1'), false); //Check file toEnd() creates does not already exist
 'hello '.toEnd('tmp/toEnd1');
-'world'.toEnd('tmp/toEnd1');
+assert.equal(fs.existsSync('tmp/toEnd1'), true); //Check that file was created
+'world'.toEnd('tmp/toEnd1'); //Write some more to the file
 var result = shell.cat('tmp/toEnd1');
 assert.equal(shell.error(), null);
-assert.equal(result, 'hello world');
+assert.equal(result, 'hello world'); //Check that the result is what we expect
 
 shell.exit(123);
