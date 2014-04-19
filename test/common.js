668bd168c7f1b2eb91bead3ebda0888c390353e9
utensil <utensilcandel@gmail.com>
2014-04-19 21:48:13 +0800

    add test for common.expand() and pass it

---
+++ test/common.js
@@ -0,0 +1,41 @@
+var shell = require('..');
+var common = require('../src/common');
+
+var assert = require('assert');
+
+shell.config.silent = true;
+
+shell.rm('-rf', 'tmp');
+shell.mkdir('tmp');
+
+//
+// Invalids
+//
+
+// too few args
+assert.throws(function () {
+  common.expand();
+}, TypeError);
+
+// should be a list
+assert.throws(function () {
+  common.expand("resources");
+}, TypeError);
+
+//
+// Valids
+//
+
+// single file, array syntax
+var result = common.expand(['resources/file1.txt']);
+assert.equal(shell.error(), null);
+assert.deepEqual(result, ['resources/file1.txt']);
+
+// multiple file, glob syntax, * for file name
+var result = common.expand(['resources/file*.txt']);
+assert.equal(shell.error(), null);
+assert.deepEqual(result, ['resources/file1.txt', 'resources/file2.txt']);
+
+shell.exit(123);
+
+
