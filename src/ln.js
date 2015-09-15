825b80e7fbea643f8b0b01d0459392004ede38b5
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2015-09-15 10:36:56 -0700

    ln: Treat parameters consistently with native ln.
    Previously, ln resolved input paths to abspaths, moving the root
    of relative paths to $PWD, whereas native `ln` treats the source
    path as relative to the _destination_. This restores the latter
    behaviour.

--- src/ln.js
+++ src/ln.js
@@ -29,7 +29,7 @@ function _ln(options, source, dest) {
     common.error('Missing <source> and/or <dest>');
   }
 
-  source = path.resolve(process.cwd(), String(source));
+  source = String(source);
   dest = path.resolve(process.cwd(), String(dest));
 
   if (!fs.existsSync(source)) {
