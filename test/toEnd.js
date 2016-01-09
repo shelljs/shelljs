18df9540ab604a1a94f53c4782caa732ce974355
Timothy Gu <timothygu99@gmail.com>
2016-01-08 21:30:56 -0800

    Make to and toEnd chainable

--- test/toEnd.js
+++ test/toEnd.js
@@ -22,11 +22,15 @@ assert.ok(shell.error());
 //
 
 assert.equal(fs.existsSync('tmp/toEnd1'), false); //Check file toEnd() creates does not already exist
+assert.equal(fs.existsSync('tmp/toEnd2'), false);
 'hello '.toEnd('tmp/toEnd1');
 assert.equal(fs.existsSync('tmp/toEnd1'), true); //Check that file was created
-'world'.toEnd('tmp/toEnd1'); //Write some more to the file
+'world'.toEnd('tmp/toEnd1').toEnd('tmp/toEnd2'); //Write some more to the file
 var result = shell.cat('tmp/toEnd1');
 assert.equal(shell.error(), null);
 assert.equal(result, 'hello world'); //Check that the result is what we expect
+result = shell.cat('tmp/toEnd2');
+assert.equal(shell.error(), null);
+assert.equal(result, 'world'); //Check that the result is what we expect
 
 shell.exit(123);
