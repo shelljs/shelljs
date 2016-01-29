439d933a69dc46d38a68ee17182d962611e84b6e
Ari Porad <ari@ariporad.com>
2016-01-28 18:23:19 -0800

    style(test/which): make test/which.js conform to the style guidelines
    This is an example PR for how to make PRs against #317.

--- test/which.js
+++ test/which.js
@@ -1,7 +1,8 @@
 var shell = require('..');
+var assert = require('assert');
+var fs = require('fs');
 
-var assert = require('assert'),
-    fs = require('fs');
+var result;
 
 shell.config.silent = true;
 
@@ -11,19 +12,17 @@ shell.mkdir('tmp');
 //
 // Invalids
 //
-
 shell.which();
 assert.ok(shell.error());
 
-var result = shell.which('asdfasdfasdfasdfasdf'); // what are the odds...
+result = shell.which('asdfasdfasdfasdfasdf'); // what are the odds...
 assert.equal(shell.error(), null);
 assert.equal(result, null);
 
 //
 // Valids
 //
-
-var result = shell.which('node');
+result = shell.which('node');
 assert.equal(shell.error(), null);
 assert.equal(fs.existsSync(result), true);
 
