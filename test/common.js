2d1ee4dae43e6d4d93a65b5e148f7387740cf211
Nate Fischer <ntfschr@gmail.com>
2016-01-31 15:34:01 -0800

    fix(mv): add -n option, make -f default behavior

--- test/common.js
+++ test/common.js
@@ -46,6 +46,36 @@ var result = common.expand(['**/file*.js']);
 assert.equal(shell.error(), null);
 assert.deepEqual(result.sort(), ["resources/file1.js","resources/file2.js","resources/ls/file1.js","resources/ls/file2.js"].sort());
 
+// common.parseOptions (normal case)
+var result = common.parseOptions('-Rf', {
+  'R': 'recursive',
+  'f': 'force',
+  'r': 'reverse'
+});
+assert.ok(result.recursive === true);
+assert.ok(result.force === true);
+assert.ok(result.reverse === false);
+
+// common.parseOptions (with mutually-negating options)
+var result = common.parseOptions('-f', {
+  'n': 'no_force',
+  'f': '!no_force',
+  'R': 'recursive'
+});
+assert.ok(result.recursive === false);
+assert.ok(result.no_force === false);
+assert.ok(result.force === undefined); // this key shouldn't exist
+
+// common.parseOptions (the last of the conflicting options should hold)
+var result = common.parseOptions('-fn', {
+  'n': 'no_force',
+  'f': '!no_force',
+  'R': 'recursive'
+});
+assert.ok(result.recursive === false);
+assert.ok(result.no_force === true);
+assert.ok(result.force === undefined); // this key shouldn't exist
+
 shell.exit(123);
 
 
