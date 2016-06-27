95fdfcde04f847bc982663e31588344868443e31
Shubham Joshi <shubhamjoshi@ucla.edu>
2016-06-27 15:02:56 +0530

    Cleaned up the pipeline

--- src/uniq.js
+++ src/uniq.js
@@ -44,26 +44,24 @@ function _uniq(options, input, output) {
               trimRight().
               split(/\r*\n/);
 
-  //Perform uniq -c on the input
-  var uniqed = [{count: 1, ln: lines[0]}];
-  lines.slice(1).forEach(function(line){
-    var cmp = options.ignoreCase ? 
-                line.toLocaleLowerCase().localeCompare(uniqed[uniqed.length-1].ln.toLocaleLowerCase()) :
-                line.localeCompare(uniqed[uniqed.length-1].ln);
-    if(cmp !== 0){
-      uniqed.push({count: 1, ln: line});
-    }else{
-      uniqed[uniqed.length - 1].count++;
-    }
-  });
-  uniqed = uniqed.
-             filter(function(obj){
-               //Do we want only duplicated objects?
-               return options.duplicates ? obj.count > 1 : true;
-             }).map(function(obj){
-               //Are we tracking the counts of each line?
-               return (options.count ? (lpad(7,obj.count) + ' ') : '') + obj.ln;
-             }).join('\n') + '\n';
+  var compare = function(a, b){
+    return options.ignoreCase ? 
+             a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase()) :
+             a.localeCompare(b);
+  }
+  var uniqed = lines.reduceRight(function(res, e){
+                 //Perform uniq -c on the input
+                 return res.length === 0 ? [{count: 1, ln: e}] :
+                        (compare(res[0].ln,e) === 0 ? 
+                          [{count: res[0].count + 1, ln: e}].concat(res.slice(1)) :
+                          [{count: 1, ln: e}].concat(res))
+               }, []).filter(function(obj){
+                 //Do we want only duplicated objects?
+                 return options.duplicates ? obj.count > 1 : true;
+               }).map(function(obj){
+                 //Are we tracking the counts of each line?
+                 return (options.count ? (lpad(7,obj.count) + ' ') : '') + obj.ln;
+               }).join('\n') + '\n';
 
   var res = new common.ShellString(uniqed, common.state.error, common.state.errorCode);
   if(output){
