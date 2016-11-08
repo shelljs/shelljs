e6e5ab77269d389f2bf3404cd7a92695c1c76efb
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2016-11-08 10:18:38 -0500

    Use shx grep to make pipe tests portable.
    Discussed in #550. To test piping and its interaction with
    real processes, a shellout is necessary. But on Windows,
    you cannot rely on utilities like grep(1) being around.
    Using shx, we can write portable code. Otherwise, the
    tests have to be conditional on the platform and be
    way more complicated.

--- test/pipe.js
+++ test/pipe.js
@@ -49,63 +49,16 @@ result = shell.sort('resources/uniq/pipe').uniq('-c').sort('-n');
 assert.equal(shell.error(), null);
 assert.equal(result.toString(), shell.cat('resources/uniq/pipeSorted').toString());
 
-// Synchronous exec
-var findCommand;
-if (process.platform === 'win32') {
-  // On Windows, we use the FIND command. FIND is semantically similar
-  // to POSIX’s grep(1) except it only matches string literals. Also,
-  // FIND requires the first argument, the search string, to be
-  // wrapped in double quotes when accessed via GetCommandLine().
-  // https://technet.microsoft.com/en-us/library/bb490906.aspx
-
-  // Get the full path to FIND. If we just exec('find'), Windows will
-  // try to run ./find.js with Windows Script Host.
-  findCommand = '"' + shell.which('FIND') + '"';
-
-  // Ensure the user does not have the wrong version of FIND. I.e.,
-  // require the following to return error.
-  result = shell.exec(findCommand + ' . -name no');
-  if (!shell.error()) {
-    console.error('Warning: Cannot verify piped exec: Found POSIX-like find(1) in PATH. This test assumes a Windows environment with its FIND command (fix your PATH, exit cygwin/mingw32/MSYS2).');
-  } else {
-    result = shell.cat('resources/grep/file').exec(findCommand + ' "alph"');
-    assert.ok(!shell.error());
-    assert.equal(result, 'alphaaaaaaabeta\r\nalphbeta\r\n');
-  }
-} else {
-  if (shell.which('grep').stdout) {
-    result = shell.cat('resources/grep/file').exec("grep 'alpha*beta'");
-    assert.ok(!shell.error());
-    assert.equal(result, 'alphaaaaaaabeta\nalphbeta\n');
-  } else {
-    console.error('Warning: Cannot verify piped exec');
-  }
-}
+// Synchronous exec. To support Windows, the arguments must be passed
+// using double quotes because node, following win32 convention,
+// passes single quotes through to process.argv verbatim.
+result = shell.cat('resources/grep/file').exec('shx grep "alpha*beta"');
+assert.ok(!shell.error());
+assert.equal(result, 'alphaaaaaaabeta\nalphbeta\n');
 
 // Async exec
-if (process.platform === 'win32') {
-  // Ensure the user does not have the wrong version of FIND. I.e.,
-  // require the following to return error.
-  result = shell.exec(findCommand + ' . -name no');
-  if (!shell.error()) {
-    console.error('Warning: Cannot verify piped exec: Found POSIX-like find(1) in PATH. This test assumes a Windows environment with its FIND command (fix your PATH, exit cygwin/mingw32/MSYS2).');
-    shell.exit(123);
-  } else {
-    shell.cat('resources/grep/file').exec(findCommand + ' "alph"', function (code, stdout) {
-      assert.equal(code, 0);
-      assert.equal(stdout, 'alphaaaaaaabeta\r\nalphbeta\r\n');
-      shell.exit(123);
-    });
-  }
-} else {
-  if (shell.which('grep').stdout) {
-    shell.cat('resources/grep/file').exec("grep 'alpha*beta'", function (code, stdout) {
-      assert.equal(code, 0);
-      assert.equal(stdout, 'alphaaaaaaabeta\nalphbeta\n');
-      shell.exit(123);
-    });
-  } else {
-    console.error('Warning: Cannot verify piped exec');
-    shell.exit(123);
-  }
-}
+shell.cat('resources/grep/file').exec('shx grep "alpha*beta"', function (code, stdout) {
+  assert.equal(code, 0);
+  assert.equal(stdout, 'alphaaaaaaabeta\nalphbeta\n');
+  shell.exit(123);
+});
