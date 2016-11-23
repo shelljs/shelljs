7dda1557f320bab20116cafdb2b03ec10773b73e
Nate Fischer <ntfschr@google.com>
2016-11-22 22:12:03 -0800

    test: refactor dirs tests to AVA

---
+++ ava-test/dirs.js
@@ -0,0 +1,39 @@
+import path from 'path';
+
+import test from 'ava';
+
+import shell from '..';
+
+test.beforeEach(() => {
+  shell.config.silent = true;
+  shell.pushd('resources/pushd');
+  shell.pushd('a');
+});
+
+//
+// Valids
+//
+
+const trail = [
+  path.resolve(path.resolve(), 'resources/pushd/a'),
+  path.resolve(path.resolve(), 'resources/pushd'),
+  path.resolve(),
+];
+
+test('no arguments', t => {
+  t.deepEqual(shell.dirs(), trail);
+});
+
+test('Single items', t => {
+  t.is(shell.dirs('+0'), trail[0]);
+  t.is(shell.dirs('+1'), trail[1]);
+  t.is(shell.dirs('+2'), trail[2]);
+  t.is(shell.dirs('-0'), trail[2]);
+  t.is(shell.dirs('-1'), trail[1]);
+  t.is(shell.dirs('-2'), trail[0]);
+});
+
+test('Clearing items', t => {
+  t.deepEqual(shell.dirs('-c'), []);
+  t.falsy(shell.error());
+});
