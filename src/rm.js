c8cb8b9f42450f193d75d7ac57cd1ba9ac411040
sergey <s.miroshnikov@brander.ua>
2016-11-24 18:50:47 +0200

    Fix: error on getting file stats without permissions

--- src/rm.js
+++ src/rm.js
@@ -25,7 +25,12 @@ function rmdirSyncRecursive(dir, force) {
   // Loop through and delete everything in the sub-tree after checking it
   for (var i = 0; i < files.length; i++) {
     var file = dir + '/' + files[i];
-    var currFile = fs.lstatSync(file);
+    try {
+      var currFile = fs.lstatSync(file);
+    } catch (e) {
+      common.error('could not get file stats (code ' + e.code + '): ' + file, { continue: true });
+      return ;
+    }
 
     if (currFile.isDirectory()) { // Recursive function back to the beginning
       rmdirSyncRecursive(file, force);
