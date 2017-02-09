2f8ce1be809ba2d74b232f97cb01e3021096a7d6
Stanislav Termosa <termosa.stanislav@gmail.com>
2017-02-09 06:44:04 +0200

    chore(test): check which -a has same result as regular one

--- test/which.js
+++ test/which.js
@@ -46,15 +46,27 @@ test('Windows can search with or without a .exe extension', t => {
   }
 });
 
-test('Search all of matched binaries shoud return an array', t => {
-  const result = shell.which('-a', 'node');
+test('Searching with -a flag returns an array', t => {
+  const commandName = 'node'; //Should be an existing command
+  const result = shell.which('-a', commandName);
   t.falsy(shell.error());
   t.truthy(result);
-  t.truthy(result.length);
+  t.not(result.length, 0);
 });
 
-test('Search all for not exist command should return an empty array', t => {
+test('Searching with -a flag for not existing command returns an empty array', t => {
   const notExist = '6ef25c13209cb28ae465852508cc3a8f3dcdc71bc7bcf8c38379ba38me';
   const result = shell.which('-a', notExist);
-  t.truthy(result.length === 0)
+  t.falsy(shell.error());
+  t.is(result.length, 0)
+});
+
+test('Searching with -a flag returns an array with first item equals to the regular search', t => {
+  const commandName = 'node'; //Should be an existing command
+  const resultForWhich = shell.which(commandName);
+  const resultForWhichA = shell.which('-a', commandName);
+  t.falsy(shell.error());
+  t.truthy(resultForWhich);
+  t.truthy(resultForWhichA);
+  t.is(resultForWhich.toString(), resultForWhichA[0].toString());
 });
