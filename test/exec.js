77db1625113e513c8d7b31ed2abcded32643c127
Nate Fischer <ntfschr@google.com>
2017-06-17 18:07:08 -0700

    Fix lint

--- test/exec.js
+++ test/exec.js
@@ -201,7 +201,7 @@ test.cb('callback as 3rd argument (silent:true)', t => {
 });
 
 test.cb('command that fails', t => {
-  shell.exec(`shx cp onlyOneCpArgument.txt`, { silent: true }, (code, stdout, stderr) => {
+  shell.exec('shx cp onlyOneCpArgument.txt', { silent: true }, (code, stdout, stderr) => {
     t.is(code, 1);
     t.is(stdout, '');
     t.is(stderr, 'cp: missing <source> and/or <dest>\n');
