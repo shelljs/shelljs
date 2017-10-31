6189d7f8e54456918520b469ec5074dd4c08875b
Nate Fischer <ntfschr@gmail.com>
2017-10-31 15:51:18 -0700

    Remove codeFile parameter (#791)
    This parameter isn't needed, we can easily rely on exit code status for this.
    
    Eliminating the parameter reduces file IO, code complexity, and removes a busy
    loop.
    
    This also removes some legacy code related to streams.
    
    Issue #782

--- src/exec-child.js
+++ src/exec-child.js
@@ -15,29 +15,25 @@ var execOptions = params.execOptions;
 var pipe = params.pipe;
 var stdoutFile = params.stdoutFile;
 var stderrFile = params.stderrFile;
-var codeFile = params.codeFile;
 
 var c = childProcess.exec(cmd, execOptions, function (err) {
   if (!err) {
-    fs.writeFileSync(codeFile, '0');
+    process.exitCode = 0;
   } else if (err.code === undefined) {
-    fs.writeFileSync(codeFile, '1');
+    process.exitCode = 1;
   } else {
-    fs.writeFileSync(codeFile, err.code.toString());
+    process.exitCode = err.code;
   }
 });
 
 var stdoutStream = fs.createWriteStream(stdoutFile);
 var stderrStream = fs.createWriteStream(stderrFile);
 
-c.stdout.pipe(stdoutStream, { end: false });
-c.stderr.pipe(stderrStream, { end: false });
+c.stdout.pipe(stdoutStream);
+c.stderr.pipe(stderrStream);
 c.stdout.pipe(process.stdout);
 c.stderr.pipe(process.stderr);
 
 if (pipe) {
   c.stdin.end(pipe);
 }
-
-c.stdout.on('end', stdoutStream.end);
-c.stderr.on('end', stderrStream.end);
