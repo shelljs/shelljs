df4cdafc06758a14341ada66203a72ac8b2c2882
dwi2 <dwi2@dwi2.com>
2018-05-17 23:13:44 +0900

    Use resources/cp/file1 and add sleep to workaround possible racing

--- test/cp.js
+++ test/cp.js
@@ -805,8 +805,8 @@ test('cp -R should be able to copy a readonly src. issue #98; (Non window platfo
 });
 
 test('cp -p should preserve mode, ownership, and timestamp', t => {
-  const result = shell.cp('-p', 'test/resources/file1', `${t.context.tmp}/preservedFile1`);
-  const stat = common.statFollowLinks('test/resources/file1');
+  const result = shell.cp('-p', 'test/resources/cp/file1', `${t.context.tmp}/preservedFile1`);
+  const stat = common.statFollowLinks('test/resources/cp/file1');
   const statOfResult = common.statFollowLinks(`${t.context.tmp}/preservedFile1`);
 
   t.is(result.code, 0);
@@ -819,6 +819,7 @@ test('cp -p should preserve mode, ownership, and timestamp', t => {
 
 test('cp -p should preserve mode, ownership, and timestamp of symlink', t => {
   const result = shell.cp('-p', 'test/resources/link', `${t.context.tmp}/copiedLink`);
+  utils.sleep(1000);
   const stat = common.statFollowLinks('test/resources/link');
   const statOfResult = common.statFollowLinks(`${t.context.tmp}/copiedLink`);
 
