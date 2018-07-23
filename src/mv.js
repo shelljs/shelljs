24d39995c0d5c2fdbbc92dce7b36bfbf8d408c1e
Vasili Galka <vgalka@sealights.io>
2018-07-23 11:40:52 +0300

    Report forgotten errors from 'mv'.

--- src/mv.js
+++ src/mv.js
@@ -110,6 +110,8 @@ function _mv(options, sources, dest) {
         // exception to pass up to the top level.
         cp('-r', src, thisDest);
         rm('-rf', src);
+      } else {
+          common.error(e.message, { continue: true });
       }
     }
   }); // forEach(src)
