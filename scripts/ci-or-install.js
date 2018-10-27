822aad87d6f06412dcf7619bc04b449aecf8f49b
Nate Fischer <ntfschr@google.com>
2018-10-27 02:02:32 -0700

    switch to execsync

--- scripts/ci-or-install.js
+++ scripts/ci-or-install.js
@@ -3,8 +3,9 @@ var childProcess = require('child_process');
 // Note: can't use 3P modules or shelljs, because this must run before we
 // download dependencies.
 
-// Also, we must use { shell: true } because `npm` is a batch script on Windows,
-// which must run in-process in the shell.
+// Also, we must use exec{Sync} because `npm` is a batch script on Windows,
+// which must run in-process in the shell, and the 'shell' option isn't
+// supported on node v4.
 
 function Version(components) {
   this.components = components;
@@ -22,11 +23,10 @@ Version.prototype.isAtLeast = function (other) {
   return true;
 };
 
-var npmVersionComponents = childProcess.spawnSync('npm', ['--version'], {
-  shell: true,
-}).stdout.toString().trim().split('.').map(function (str) {
-  return parseInt(str, 10);
-});
+var npmVersionComponents = childProcess.execSync('npm --version')
+    .toString().trim().split('.').map(function (str) {
+      return parseInt(str, 10);
+    });
 var npmVersion = new Version(npmVersionComponents);
 var minimumVersionWithNpmCi = new Version([5, 7, 0]);
 
@@ -36,4 +36,4 @@ var subcommand = npmVersion.isAtLeast(minimumVersionWithNpmCi) ?
 
 console.log('Executing `npm ' + subcommand + '`');
 // Async. Node waits until this is finished.
-childProcess.spawn('npm', [subcommand], { shell: true, stdio: 'inherit' });
+childProcess.exec('npm ' + subcommand, { stdio: 'inherit' });
