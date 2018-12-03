e6067068559478d6b4fb735414a834f82f59b701
Nate Fischer <ntfschr@gmail.com>
2018-12-02 21:52:54 -0800

    fix(exec): consistent error message for maxBuffer (#919)
    * fix(exec): consistent error message for maxBuffer
    
    This explicitly checks for maxBuffer errors in exec-child.js and
    provides a consistent error message. This modifies the test to verify
    this.
    
    This also supports the change in Node v10, which emits this as a
    RangeError instead of a regular Error.
    
    Although the error message is now explicit, this is not part of our API,
    since it's just tacked onto the end of exec's stderr.
    
    Fixes #915
    
    * Remove debugging log, ignore uncovered lines

--- src/exec-child.js
+++ src/exec-child.js
@@ -16,18 +16,48 @@ var pipe = params.pipe;
 var stdoutFile = params.stdoutFile;
 var stderrFile = params.stderrFile;
 
+function isMaxBufferError(err) {
+  var maxBufferErrorPattern = /^.*\bmaxBuffer\b.*exceeded.*$/;
+  if (err instanceof Error && err.message &&
+        err.message.match(maxBufferErrorPattern)) {
+    // < v10
+    // Error: stdout maxBuffer exceeded
+    return true;
+  } else if (err instanceof RangeError && err.message &&
+        err.message.match(maxBufferErrorPattern)) {
+    // >= v10
+    // RangeError [ERR_CHILD_PROCESS_STDIO_MAXBUFFER]: stdout maxBuffer length
+    // exceeded
+    // TODO(nfischer): remove when we add v10 CI (Github issue #856).
+    /* istanbul ignore next */
+    return true;
+  }
+  return false;
+}
+
+var stdoutStream = fs.createWriteStream(stdoutFile);
+var stderrStream = fs.createWriteStream(stderrFile);
+
+function appendError(message, code) {
+  stderrStream.write(message);
+  process.exitCode = code;
+}
+
 var c = childProcess.exec(cmd, execOptions, function (err) {
   if (!err) {
     process.exitCode = 0;
+  } else if (isMaxBufferError(err)) {
+    appendError('maxBuffer exceeded', 1);
+  } else if (err.code === undefined && err.message) {
+    /* istanbul ignore next */
+    appendError(err.message, 1);
   } else if (err.code === undefined) {
-    process.exitCode = 1;
+    /* istanbul ignore next */
+    appendError('Unknown issue', 1);
   } else {
     process.exitCode = err.code;
   }
 });
-
-var stdoutStream = fs.createWriteStream(stdoutFile);
-var stderrStream = fs.createWriteStream(stderrFile);
 
 c.stdout.pipe(stdoutStream);
 c.stderr.pipe(stderrStream);
