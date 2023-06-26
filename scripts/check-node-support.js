547a739d61f6687fba475eda364f3a64b4c7bd06
Nate Fischer <ntfschr@google.com>
2023-06-26 16:14:51 -0700

    chore: update CI to test against node v20
    No change to logic. This updates GitHub actions CI to test on node v19
    and v20.

--- scripts/check-node-support.js
+++ scripts/check-node-support.js
@@ -9,7 +9,7 @@ var shell = require('..');
 
 // This is the authoritative list of supported node versions.
 var MIN_NODE_VERSION = 8;
-var MAX_NODE_VERSION = 18;
+var MAX_NODE_VERSION = 20;
 
 function checkReadme(minNodeVersion) {
   var start = '<!-- start minVersion -->';
